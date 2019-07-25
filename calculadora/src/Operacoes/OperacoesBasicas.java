1d11023c32c1c733ed10e0388da079af58f87260
Aluno <Aluno@SN-279240>
2019-07-24 21:39:13 -0300

    Segundo commit.

---
+++ calculadora/src/Operacoes/OperacoesBasicas.java
@@ -0,0 +1,28 @@
+
+package Operacoes;
+
+
+public class OperacoesBasicas {
+    
+    public double somar(double a, double b) {
+        double resultado = a + b;
+        return resultado;
+    }
+
+    public double subtrair(double a, double b) {
+        double resultado = a - b;
+        return resultado;
+    }
+
+    public double multiplicar(double a, double b) {
+        double resultado = a * b;
+        return resultado;
+    }    
+        
+
+    public double dividir(double a, double b) {
+        double resultado = a / b;
+        return resultado;
+    }
+    
+}
